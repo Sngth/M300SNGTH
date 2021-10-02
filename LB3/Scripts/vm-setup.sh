14842d6b5ec31ab7de330ebb17fdcca310fc988c
Sngth <sangeeth.sivakumaran@edu.tbz.ch>
2021-10-02 15:52:54 +0200

    02.10.2021 U5

--- LB3/Scripts/vm-setup.sh
+++ LB3/Scripts/vm-setup.sh
@@ -2,5 +2,6 @@ sudo apt update
 sudo apt install -y docker.io           # das sind 360 MB
 sudo git clone https://github.com/Sngth/M300SNGTH.git
 cd M300SNGTH
-cd LB3/docker/ubuntu
-docker build -t ubuntu .
+cd LB3/docker/apache
+docker build -t app .
+docker run -d -p 8011:80 -v /data/server/app:/var/www/site/app --name app app
