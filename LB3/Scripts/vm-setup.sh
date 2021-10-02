c1a8a60265aeda719a14964b7a47e813bcb34338
Sngth <sangeeth.sivakumaran@edu.tbz.ch>
2021-10-02 17:30:42 +0200

    02.10.2021 U7

--- LB3/Scripts/vm-setup.sh
+++ LB3/Scripts/vm-setup.sh
@@ -2,6 +2,5 @@ sudo apt update
 sudo apt install -y docker.io           # das sind 360 MB
 sudo git clone https://github.com/Sngth/M300SNGTH.git
 cd M300SNGTH
-cd LB3/docker/apache
-docker build -t app .
-docker run -d -p 8011:80 -v /data/server/app:/var/www/site/app --name app app
+cd LB3/docker/wordpress
+sudo docker-compose up -d
