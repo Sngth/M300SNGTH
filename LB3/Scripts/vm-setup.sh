65e0fa3a113f8c7824374154bcae4beadcf6d1dd
Sngth <sangeeth.sivakumaran@edu.tbz.ch>
2021-10-02 13:10:35 +0200

    02.10.2021 U3

--- LB3/Scripts/vm-setup.sh
+++ LB3/Scripts/vm-setup.sh
@@ -3,5 +3,5 @@ sudo apt install -y docker.io           # das sind 360 MB
 sudo git clone https://github.com/Sngth/M300SNGTH.git
 cd M300SNGTH
 cd LB3/docker/apache
-docker build -t apache .
-docker run --rm -d -p 8011:80 -v `pwd`/web:/var/www/html --name apache apache
+sudo docker build -t apache .
+sudo docker run --rm -d -p 8011:80 -v `pwd`/web:/var/www/html --name apache apache
