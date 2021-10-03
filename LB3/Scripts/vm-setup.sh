0ab769338c377fa5bf7b096ea492a1dd46aefe40
Sngth <sangeeth.sivakumaran@edu.tbz.ch>
2021-10-03 13:28:05 +0200

    03.10.2021 U9

--- LB3/Scripts/vm-setup.sh
+++ LB3/Scripts/vm-setup.sh
@@ -1,5 +1,6 @@
 sudo apt update
 sudo apt install -y docker.io           # das sind 360 MB
+sudo apt-get install docker-compose -y
 #sudo git clone https://github.com/Sngth/M300SNGTH.git
 #cd M300SNGTH
 #cd LB3/docker/mysql
