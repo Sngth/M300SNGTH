f94ccf890f4be3f8bf5f77bf82380ba3030222fc
Sngth <sangeeth.sivakumaran@edu.tbz.ch>
2021-10-03 18:10:53 +0200

    03.10.2021 U11

--- LB3/Scripts/vm-setup.sh
+++ LB3/Scripts/vm-setup.sh
@@ -2,5 +2,5 @@ sudo apt update
 sudo apt install -y docker.io           # das sind 360 MB
 sudo apt-get install docker-compose -y
 sudo git clone https://github.com/Sngth/M300SNGTH.git
-cd M300SNGTH/LB3/docker/nextcloud
+cd M300SNGTH/LB3/docker/webmail
 #sudo docker run -t -p 8011:80 
